775206d9b6fc02304d2b4dab1e88145f91ebe192
Marius Turcan <mariusturcan123@gmail.com>
2025-01-18 21:17:27 +0200

    Header plus some style

---
+++ assets/data/json/ContactsInfo.js
@@ -0,0 +1,5 @@
+export const ContactsInfo = {
+    email: 'info@example.com',
+    phone: '+91 7052 101 786',
+    address: '1247/Plot No. 39, 15th Phase, LHB Colony, Kanpur',
+}
---
+++ assets/data/json/HeaderNav.js
@@ -0,0 +1,26 @@
+export const HeaderNav = [
+    {
+        name: "Home",
+        link: "/",
+    },
+    {
+        name: "About us",
+        link: "/about.html",
+    },
+    {
+        name: "Our taxi",
+        link: "/our-taxi.html",
+    },
+    {
+        name: "Pricing",
+        link: "/pricing.html",
+    },
+    {
+        name: "FAQ",
+        link: "/faq.html",
+    },
+    {
+        name: "Contact us",
+        link: "/contact.html",
+    },
+];
