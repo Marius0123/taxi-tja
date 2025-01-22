7891e8d0a235b37d10fef27e9345523637f2dc81
Marius Turcan <mariusturcan123@gmail.com>
2025-01-22 20:47:04 +0200

    Contacts page done

--- assets/script/components/blocks/Contacts_page/Contacts.js
+++ assets/script/components/blocks/Contacts_page/Contacts.js
@@ -1,11 +1,12 @@
 import { GetCarAdd } from "../../elements/main/GetCarAdd.js";
 import { PageHead } from "../../elements/main/PageHead.js";
+import { ContactsBlock } from "./ContactsBlock.js";
 
 const main = document.querySelector("main");
 
 const ContactsContent = 
             `${PageHead}
-            
+            ${ContactsBlock}
             ${GetCarAdd}`;
         
 const Contacts = ContactsContent;
