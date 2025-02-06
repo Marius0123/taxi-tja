f8338e8acbb9e0577c66b481315b437b2d77e2d1
Marius Turcan <mariusturcan123@gmail.com>
2025-02-06 20:33:23 +0200

    Testimonials action done

---
+++ assets/data/json/Testimonials.data.js
@@ -0,0 +1,102 @@
+export const TestimonialsData = [
+    {
+        photo: {
+            type: 'svg',
+            content: 
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/> </svg>',
+        },
+        author: 'Jone Walker',
+        car: 'Toyota Camry (1982)',
+        rating: 4,
+        desc: 'It is a long established fact reader will be distracted by the readable content of a page when looking at its layout. Many desktop publihing packages and web pagejk editors now use Lorem',
+    },
+    {
+        photo: {
+            type: 'svg',
+            content: 
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/> </svg>',
+        },
+        author: 'Will Kolder',
+        car: 'Toyota Camry (1982)',
+        rating: 5,
+        desc: 'It is a long established fact reader will be distracted by the readable content of a page when looking at its layout. Many desktop publihing packages and web pagejk editors now use Lorem',
+    },
+    {
+        photo: {
+            type: 'svg',
+            content: 
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/> </svg>',
+        },
+        author: 'Joe Smith',
+        car: 'Toyota Camry (1982)',
+        rating: 5,
+        desc: 'It is a long established fact reader will be distracted by the readable content of a page when looking at its layout. Many desktop publihing packages and web pagejk editors now use Lorem',
+    },
+    {
+        photo: {
+            type: 'svg',
+            content: 
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/> </svg>',
+        },
+        author: 'Sam Black',
+        car: 'Toyota Camry (1982)',
+        rating: 5,
+        desc: 'It is a long established fact reader will be distracted by the readable content of a page when looking at its layout. Many desktop publihing packages and web pagejk editors now use Lorem',
+    },
+    {
+        photo: {
+            type: 'svg',
+            content: 
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/> </svg>',
+        },
+        author: 'Daniel Hondo Harrelson',
+        car: 'Toyota Camry (1982)',
+        rating: 5,
+        desc: 'It is a long established fact reader will be distracted by the readable content of a page when looking at its layout. Many desktop publihing packages and web pagejk editors now use Lorem',
+    },
+    {
+        photo: {
+            type: 'svg',
+            content: 
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/> </svg>',
+        },
+        author: 'Derek Morgen',
+        car: 'Toyota Camry (1982)',
+        rating: 5,
+        desc: 'It is a long established fact reader will be distracted by the readable content of a page when looking at its layout. Many desktop publihing packages and web pagejk editors now use Lorem',
+    },
+    {
+        photo: {
+            type: 'svg',
+            content: 
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/> </svg>',
+        },
+        author: 'Daniel Hondo Harrelson',
+        car: 'Toyota Camry (1982)',
+        rating: 5,
+        desc: 'It is a long established fact reader will be distracted by the readable content of a page when looking at its layout. Many desktop publihing packages and web pagejk editors now use Lorem',
+    },
+    {
+        photo: {
+            type: 'svg',
+            content: 
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/> </svg>',
+        },
+        author: 'Derek Morgen',
+        car: 'Toyota Camry (1982)',
+        rating: 5,
+        desc: 'It is a long established fact reader will be distracted by the readable content of a page when looking at its layout. Many desktop publihing packages and web pagejk editors now use Lorem',
+    },
+    {
+        photo: {
+            type: 'svg',
+            content: 
+                    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"> <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/> </svg>',
+        },
+        author: 'Jone Walker',
+        car: 'Toyota Camry (1982)',
+        rating: 4,
+        desc: 'It is a long established fact reader will be distracted by the readable content of a page when looking at its layout. Many desktop publihing packages and web pagejk editors now use Lorem',
+    },
+    
+];
